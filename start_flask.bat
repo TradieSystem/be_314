9e5c0e5fada1f25e1a424c0fa6046d954f213747
Benjamin Oshana <bo562@uowmail.edu.au>
2023-05-01 14:44:40 +1000

    Update start_flask.bat

--- start_flask.bat
+++ start_flask.bat
@@ -1,3 +1,3 @@
 cd ./src/flask
-call /venv/Scripts/activate.bat && python -m pip install -r requirements.txt && python app.py
+call /venv/Scripts/activate.bat && python -m pip install -r requirements.txt && python -m pip install -e ./../py && python app.py
 PAUSE
