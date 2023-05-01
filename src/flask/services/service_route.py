7e89f6385bfe689069a9529ce43cbd789b8ac809
Benjamin Oshana <bo562@uowmail.edu.au>
2023-05-01 15:37:52 +1000

    Update service_route.py

--- src/flask/services/service_route.py
+++ src/flask/services/service_route.py
@@ -18,7 +18,7 @@ service_base = "serviceRequest"
 @cross_origin()
 def serviceRequestGet():
     result = Service_Controller.Event_Start(Request_Construct.construct_request(request))
-    
+
     if result is None:
         return {}
 
@@ -50,6 +50,10 @@ def availableServiceRequest():
 @cross_origin()
 def applicationCreate():
     result = Service_Controller.Event_Start(Request_Construct.construct_request(request))
+
+    if result is None:
+        return {}
+    
     return result
 
 
