6f32fe699f95d9344e06204b8b4e8e455481c0bc
Benjamin Oshana <bo562@uowmail.edu.au>
2023-05-01 14:06:33 +1000

    Fixed issue with flask script for mac

--- start_flask.sh
+++ start_flask.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 cd ./src/flask
-call /venv/Scripts/activate.bat 
+. ./venv/Scripts/activate
+python -m pip install -r requirements.txt
+python -m pip install -e ./../py
 python app.py
+read -p "Press any key to continue..."
