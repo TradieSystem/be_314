3642cff6c5268c0709c9fc0774eef7d8b8005c4e
Veronica <veronica.nashed@gmail.com>
2023-05-04 18:34:01 +1000

    Update to mac script to run with python3

--- start_flask.sh
+++ start_flask.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 cd ./src/flask
 . ./venv/Scripts/activate
-python -m pip install -r requirements.txt
-python -m pip install -e ./../py
-python app.py
+python3 -m pip install -r requirements.txt
+python3 -m pip install -e ./../py
+python3 app.py
 read -p "Press any key to continue..."
