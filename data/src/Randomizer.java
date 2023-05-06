9bae3ea5e78fbb2a330e8345fa07b7f455d318ae
Benjamin Oshana <bo562@uowmail.edu.au>
2023-05-06 21:06:13 +1000

    Set up Initial Java Project

---
+++ data/src/Randomizer.java
@@ -0,0 +1,14 @@
+import data_types.RandomUser;
+import java.util.Random;
+
+public class Randomizer {
+    private Random random;
+
+    public Randomizer() {
+        random = new Random();
+    }
+
+    public RandomUser GenerateUser() {
+        return null;
+    }
+}
---
+++ data/src/data_types/RandomUser.java
@@ -0,0 +1,9 @@
+package data_types;
+
+public class RandomUser {
+    private String first_name;
+    private String last_name;
+    private String email_address;
+    private String mobile;
+    private String password;
+}
