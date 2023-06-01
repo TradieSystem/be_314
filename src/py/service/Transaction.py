a80433b99604bdda486211961a731dad7d605c59
Benjamin Oshana <126941577+bo562@users.noreply.github.com>
2023-06-01 19:12:35 +1000

    Transaction was missing due to some merging errors

--- src/py/service/Transaction.py
+++ src/py/service/Transaction.py
@@ -4,6 +4,17 @@ py that describes transaction object, handles all functionality to do with trans
 from datetime import datetime
 from dataclasses import dataclass
 
+from mysql.connector import errors
+
+from service.Request import Request
+from util.database.Database import Database
+from util.database.DatabaseLookups import DatabaseLookups
+from util.database.DatabaseStatus import DatabaseStatus
+from util.handling.errors.database.DatabaseConnectionError import DatabaseConnectionError
+from util.handling.errors.database.DatabaseObjectAlreadyExists import DatabaseObjectAlreadyExists
+from util.handling.errors.database.FailedToCreateDatabaseObject import FailedToCreateDatabaseObject
+
+
 @dataclass
 class Transaction:
     transaction_id: int
@@ -11,16 +22,94 @@ class Transaction:
     transaction_date: datetime = None
     transaction_status: str = None
     user_id: int = None
-    billing_type: str = None
+    billing_type_id: int = None
     billing_id: int = None
 
-    def create_transaction(self, cost: float, transaction_date: datetime, transaction_status: str,
-                           user_id: int, billing_type: str, billing_id: int):
-        pass
+    def create_transaction(self, user_id: int):
+        database = Database.database_handler(DatabaseLookups.User)  # create database connection
 
-    def update_transaction(self, transaction_id: int, cost: float, transaction_date: datetime, transaction_status: str,
-                           user_id: int, billing_type: str, billing_id: int):
-        pass
+        # check if database is connected, if not connect
+        if database.status is DatabaseStatus.Disconnected:
+            database.connect()
+
+        elif database.status is DatabaseStatus.NoImplemented:
+            raise DatabaseConnectionError(table='transaction', query=None, database_object=None)
+
+        # if user_id not populated
+        if self.user_id is None:
+            self.user_id = user_id
+
+        # construct query for creation
+        database.insert(self, 'transaction', ('transaction_id',))
+
+        # try to run query
+        try:
+            self.transaction_id = database.run()
+            database.commit()
+
+        except errors.IntegrityError as ie:  # in case that change violates consistency constraints
+            # clean up instance and rollback to remove lock
+            database.rollback()
+            query = database.review_query()
+            database.clear()
+            database.disconnect()
+
+            # if there is an integrity error
+            if ie.errno == 1452:  # cannot solve gracefully
+                # raise error
+                raise DatabaseObjectAlreadyExists(table='user', query=query, database_object=self)
+
+            # some other consistency constraint check
+            raise FailedToCreateDatabaseObject(table='user', query=query, database_object=self)
+
+        # clear database tool
+        database.clear()
+        database.disconnect()
+
+        return self
+
+    def update_transaction(self, user_id: int):
+        database = Database.database_handler(DatabaseLookups.User)  # create database connection
+
+        # check if database is connected, if not connect
+        if database.status is DatabaseStatus.Disconnected:
+            database.connect()
+
+        elif database.status is DatabaseStatus.NoImplemented:
+            raise DatabaseConnectionError(table='request', query=None, database_object=None)
+
+        # if user_id not populated
+        if self.user_id is None:
+            self.user_id = user_id
+
+        # construct query for creation
+        database.update(self, 'transaction', ('transaction_id',))
+
+        # try to run query
+        try:
+            self.transaction_id = database.run()
+            database.commit()
+
+        except errors.IntegrityError as ie:  # in case that change violates consistency constraints
+            # clean up instance and rollback to remove lock
+            database.rollback()
+            query = database.review_query()
+            database.clear()
+            database.disconnect()
+
+            # if there is an integrity error
+            if ie.errno == 1452:  # cannot solve gracefully
+                # raise error
+                raise DatabaseObjectAlreadyExists(table='user', query=query, database_object=self)
+
+            # some other consistency constraint check
+            raise FailedToCreateDatabaseObject(table='user', query=query, database_object=self)
+
+        # clear database tool
+        database.clear()
+        database.disconnect()
+
+        return self
 
     def get_transaction(self):
         pass
