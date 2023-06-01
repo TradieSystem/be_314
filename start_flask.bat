128c3579d06e1d8c3d105db34e1af49802e6180e
Benjamin Oshana <126941577+bo562@users.noreply.github.com>
2023-06-01 18:55:45 +1000

    Fixed issues with starting Flask Server

--- start_flask.bat
+++ start_flask.bat
@@ -1,3 +1,5 @@
 cd ./src/flask
-call /venv/Scripts/activate.bat && python -m pip install -r requirements.txt && python -m pip install -e ./../py && python app.py
+cd
+call ./flask/Scripts/activate.bat && python -m pip install -r requirements.txt && python -m pip install -e ./../py && python app.py
+cd
 PAUSE
